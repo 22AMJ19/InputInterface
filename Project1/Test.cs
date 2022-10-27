94b322e45660447d49643a5a8d6cc64c7e10607f
22amj19 <22amj19@ms.dendai.ac.jp>
2022-10-27 21:01:32 +0900

    10/27v1

--- Project1/Test.cs
+++ Project1/Test.cs
@@ -1,8 +1,1 @@
-﻿class Test {
-    public static void Main() {
-        string[] kanji = Ext.ToKanji("でんだい");
-        for (int i = 0; i < kanji.Length; i++) { 
-            System.Console.WriteLine(kanji[i]);
-        }
-    }
-}
+﻿
---
+++ Project1/TextTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using System.IO;
+
+class TextTest
+{
+    static void Main(string[] args)
+    {
+        //Pass the filepath and filename to the StreamWriter Constructor
+        StreamWriter sw = new StreamWriter("C:\\Test.txt");
+        //Write a line of text
+        sw.WriteLine("Hello World!!");
+        //Write a second line of text
+        //Close the file
+        sw.Close();
+    }
+}
